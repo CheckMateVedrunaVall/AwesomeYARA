f0baae2f59cd4eb4ca200538d599b9a7b7184278
CheckMateVedrunaVall <122304153+CheckMateVedrunaVall@users.noreply.github.com>
2023-01-23 16:21:34 +0100

    Update convertidor.py

--- Converter/convertidor.py
+++ Converter/convertidor.py
@@ -7,8 +7,8 @@ print(converter)
 
 char = input("Quieres mirar otra cadena en texto plano? Si <s> No <n>: ")
 while char == 's':
-    cadena2 = input("Escribe otra cadena: ")
-    converter2 = cadena2.replace("\\x00", "")
+    cadena = input("Escribe otra cadena: ")
+    converter = cadena.replace("\\x00", "")
 
     print(converter2)
 
